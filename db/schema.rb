b9d81df01a0a7e562ad55a8703fcefa8f963e357
TakumaNakahashi <115599704+tnakahashi@users.noreply.github.com>
2023-01-16 20:02:47 +0000

    [Fix]adminのログイン機能・ヘッダーを修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_14_202443) do
+ActiveRecord::Schema.define(version: 2023_01_15_135935) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -78,6 +78,15 @@ ActiveRecord::Schema.define(version: 2023_01_14_202443) do
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
+  create_table "favorites", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["customer_id"], name: "index_favorites_on_customer_id"
+    t.index ["post_id"], name: "index_favorites_on_post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.integer "customer_id", null: false
     t.string "title", null: false
@@ -93,4 +102,6 @@ ActiveRecord::Schema.define(version: 2023_01_14_202443) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "customers"
   add_foreign_key "comments", "posts"
+  add_foreign_key "favorites", "customers"
+  add_foreign_key "favorites", "posts"
 end
