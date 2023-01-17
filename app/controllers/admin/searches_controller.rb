e96963a566ebeba8d4a46b69e8f34225e48ee96c
EC2 Default User <ec2-user@ip-172-31-11-119.ap-northeast-1.compute.internal>
2023-01-17 21:33:37 +0000

    [Update]adminの検索機能追加

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -1,2 +1,12 @@
 class Admin::SearchesController < ApplicationController
+  def search
+    @range = params[:range]
+    @word = params[:word]
+
+    if @range == "ユーザ"
+      @customers = Customer.looks(params[:search], params[:word])
+    else
+      @posts = Post.looks(params[:search], params[:word])
+    end
+  end
 end
