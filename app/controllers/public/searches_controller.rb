559a088cec13158eb0fc370e5b197f50324dba09
EC2 Default User <ec2-user@ip-172-31-11-119.ap-northeast-1.compute.internal>
2023-01-21 11:45:12 +0000

    [Update]publicにて退会済み会員の投稿を検索ページにて非表示に

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -4,9 +4,10 @@ class Public::SearchesController < ApplicationController
     @word = params[:word]
 
     if @range == "ユーザ"
-      @customers = Customer.looks(params[:search], params[:word])
+      @customers = Customer.looks(params[:search], params[:word]).where.not(is_deleted: true)
     else
-      @posts = Post.looks(params[:search], params[:word]).where(is_deleted: false).published
+      customer_ids = Customer.where(is_deleted: true).pluck(:id)
+      @posts = Post.looks(params[:search], params[:word]).where(is_deleted: false).where.not(customer_id: customer_ids).published
     end
   end
 end
