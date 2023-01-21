689a4785b9cd3aa28788da2c1deec9ff36108373
EC2 Default User <ec2-user@ip-172-31-11-119.ap-northeast-1.compute.internal>
2023-01-21 23:04:19 +0000

    [Update]フォロー数を会員ページ等に追加

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -11,12 +11,14 @@ class Public::RelationshipsController < ApplicationController
   end
   # フォロー一覧
   def followings
+    customer_ids = Customer.where(is_deleted: true).pluck(:id)
     customer = Customer.find(params[:customer_id])
-    @customers = customer.followings
+    @customers = customer.followings.where.not(id: customer_ids)
   end
   # フォロワー一覧
   def followers
+    customer_ids = Customer.where(is_deleted: true).pluck(:id)
     customer = Customer.find(params[:customer_id])
-    @customers = customer.followers
+    @customers = customer.followers.where.not(id: customer_ids)
   end
 end
