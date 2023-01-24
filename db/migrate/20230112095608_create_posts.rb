b6d3aa8ebcc695e1f34142f1b10bc6f2dc4fcaf5
EC2 Default User <ec2-user@ip-172-31-11-119.ap-northeast-1.compute.internal>
2023-01-24 12:24:39 +0000

    post

--- db/migrate/20230112095608_create_posts.rb
+++ db/migrate/20230112095608_create_posts.rb
@@ -5,8 +5,8 @@ class CreatePosts < ActiveRecord::Migration[6.1]
       t.integer :genre_id, null: false
       t.string :title,  null: false
       t.text :introduction,  null: false
-      t.text :selling_point,  null: false, default: ''
-      t.text :detail,  null: false, default: ''
+      t.text :selling_point,  null: false
+      t.text :detail,  null: false
       t.integer :status, null: false, default: 0
       t.boolean :is_deleted, null: false, default: "FALSE"
       t.timestamps
