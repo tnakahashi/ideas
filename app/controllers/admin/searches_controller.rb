15a4fc750a08a04c4e40080d01400f8967b440fa
EC2 Default User <ec2-user@ip-172-31-11-119.ap-northeast-1.compute.internal>
2023-01-24 21:47:03 +0000

    [Fix]adminの検索画面にて非表示ステータスの投稿が非表示になる不具合を修正

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -8,11 +8,11 @@ class Admin::SearchesController < ApplicationController
     else
       # 新着・いいね・コメント数順の並び替え
       if params[:target] == "favorite"
-        @posts = Post.where(is_deleted: false).published.sort {|a,b| b.favorited_customers.size <=> a.favorited_customers.size}
+        @posts = Post.looks(params[:search], params[:word]).published.sort {|a,b| b.favorited_customers.size <=> a.favorited_customers.size}
       elsif params[:target] == 'comment'
-        @posts = Post.where(is_deleted: false).published.sort {|a,b| b.commented_customers.size <=> a.commented_customers.size}
+        @posts = Post.looks(params[:search], params[:word]).published.sort {|a,b| b.commented_customers.size <=> a.commented_customers.size}
       else
-        @posts = Post.where(is_deleted: false).published.order(created_at: :desc)
+        @posts = Post.looks(params[:search], params[:word]).published.order(created_at: :desc)
       end
     end
   end
