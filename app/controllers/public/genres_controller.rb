ec55b39e1b2eb5aa33661f179ebb3477391ae240
EC2 Default User <ec2-user@ip-172-31-11-119.ap-northeast-1.compute.internal>
2023-01-24 11:39:32 +0000

    [Update]投稿一覧等のレイアウト調整

--- app/controllers/public/genres_controller.rb
+++ app/controllers/public/genres_controller.rb
@@ -8,7 +8,14 @@ class Public::GenresController < ApplicationController
     @genre = Genre.find(params[:id])
     # 退会済みの会員の投稿を非表示に
     customer_ids = Customer.where(is_deleted: true).pluck(:id)
-    @posts = Post.where(is_deleted: false).where.not(customer_id: customer_ids).published
+    # 新着・いいね・コメント数順の並び替え
+    if params[:target] == "favorite"
+      @posts = Post.where(is_deleted: false).where.not(customer_id: customer_ids).published.sort {|a,b| b.favorited_customers.size <=> a.favorited_customers.size}
+    elsif params[:target] == 'comment'
+      @posts = Post.where(is_deleted: false).where.not(customer_id: customer_ids).published.sort {|a,b| b.commented_customers.size <=> a.commented_customers.size}
+    else
+      @posts = Post.where(is_deleted: false).where.not(customer_id: customer_ids).published.order(created_at: :desc)
+    end
   end
 
 end
--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -7,14 +7,12 @@ class Public::PostsController < ApplicationController
     # 退会済みの会員の投稿を非表示に
     customer_ids = Customer.where(is_deleted: true).pluck
     # 新着・いいね・コメント数順の並び替え
-    if params[:target] == "date"
-      @posts = Post.where(is_deleted: false).where.not(customer_id: customer_ids).published.order(created_at: :desc)
-    elsif params[:target] == "favorite"
+    if params[:target] == "favorite"
       @posts = Post.where(is_deleted: false).where.not(customer_id: customer_ids).published.sort {|a,b| b.favorited_customers.size <=> a.favorited_customers.size}
     elsif params[:target] == 'comment'
       @posts = Post.where(is_deleted: false).where.not(customer_id: customer_ids).published.sort {|a,b| b.commented_customers.size <=> a.commented_customers.size}
     else
-      @posts = Post.where(is_deleted: false).where.not(customer_id: customer_ids).published
+      @posts = Post.where(is_deleted: false).where.not(customer_id: customer_ids).published.order(created_at: :desc)
     end
     
     # タグ追加用
