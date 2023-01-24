2edb22268915927e7c9c7f391b23ff20e21841c4
EC2 Default User <ec2-user@ip-172-31-11-119.ap-northeast-1.compute.internal>
2023-01-24 21:43:36 +0000

    [Update]admin・publicの検索画面に並び替え機能を追加

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -6,7 +6,14 @@ class Admin::SearchesController < ApplicationController
     if @range == "ユーザ"
       @customers = Customer.looks(params[:search], params[:word])
     else
-      @posts = Post.looks(params[:search], params[:word]).published
+      # 新着・いいね・コメント数順の並び替え
+      if params[:target] == "favorite"
+        @posts = Post.where(is_deleted: false).published.sort {|a,b| b.favorited_customers.size <=> a.favorited_customers.size}
+      elsif params[:target] == 'comment'
+        @posts = Post.where(is_deleted: false).published.sort {|a,b| b.commented_customers.size <=> a.commented_customers.size}
+      else
+        @posts = Post.where(is_deleted: false).published.order(created_at: :desc)
+      end
     end
   end
 end
--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -7,7 +7,14 @@ class Public::SearchesController < ApplicationController
       @customers = Customer.looks(params[:search], params[:word]).where.not(is_deleted: true)
     else
       customer_ids = Customer.where(is_deleted: true).pluck(:id)
-      @posts = Post.looks(params[:search], params[:word]).where(is_deleted: false).where.not(customer_id: customer_ids).published
+      # 新着・いいね・コメント数順の並び替え
+      if params[:target] == "favorite"
+        @posts = Post.where(is_deleted: false).where.not(customer_id: customer_ids).published.sort {|a,b| b.favorited_customers.size <=> a.favorited_customers.size}
+      elsif params[:target] == 'comment'
+        @posts = Post.where(is_deleted: false).where.not(customer_id: customer_ids).published.sort {|a,b| b.commented_customers.size <=> a.commented_customers.size}
+      else
+        @posts = Post.where(is_deleted: false).where.not(customer_id: customer_ids).published.order(created_at: :desc)
+      end
     end
   end
 end
