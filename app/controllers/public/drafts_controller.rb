906ac841e8f1abeb798afa678dfa1e06c0d93982
EC2 Default User <ec2-user@ip-172-31-11-119.ap-northeast-1.compute.internal>
2023-01-27 15:44:17 +0000

    [Update]publicの下書きのレイアウトを調整

--- app/controllers/public/drafts_controller.rb
+++ app/controllers/public/drafts_controller.rb
@@ -6,5 +6,11 @@ class Public::DraftsController < ApplicationController
   def edit
     @post = Post.find(params[:id])
     @genres = Genre.all
+    
+    # タグ追加用
+    if params[:tag]
+      Tag.create(name: params[:tag])
+      redirect_to edit_customer_draft_path(@post)
+    end
   end
 end
