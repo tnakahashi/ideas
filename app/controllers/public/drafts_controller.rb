70951c3174cbfc735acbb97d35de283de68508fd
EC2 Default User <ec2-user@ip-172-31-11-119.ap-northeast-1.compute.internal>
2023-01-27 18:10:14 +0000

    [Fix]下書き編集後の遷移先の不具合を修正

--- app/controllers/public/drafts_controller.rb
+++ app/controllers/public/drafts_controller.rb
@@ -1,4 +1,8 @@
 class Public::DraftsController < ApplicationController
+
+  # 編集・削除を投稿者のみに制限(ensure_customerメソッド参照)
+  before_action :ensure_customer, only: [:edit]
+
   def index
     @all_drafts = Post.draft
   end
@@ -13,4 +17,26 @@ class Public::DraftsController < ApplicationController
       redirect_to edit_customer_draft_path(@post)
     end
   end
+
+  def update
+    if @post.update(post_params)
+      redirect_to post_path(@post), notice: "You have updated post successfully."
+    else
+      render :edit
+    end
+  end
+  
+
+  private
+    # ストロングパラメータ
+    def post_params
+      params.require(:post).permit(:id, :genre_id, :image, :title, :introduction, :selling_point, :detail, :status, tag_ids: [])
+    end
+
+    # 編集・削除を投稿者のみに制限
+    def ensure_customer
+      @posts = current_customer.post
+      @post = @posts.find_by(id: params[:id])
+      redirect_to posts_path unless @post
+    end
 end
