a87a2343a28ecc0a9df5e1d2d44af3c6ddcea4bc
EC2 Default User <ec2-user@ip-172-31-11-119.ap-northeast-1.compute.internal>
2023-02-18 08:21:37 +0000

    [Fix]adminの一週間のいいね数並び替えの不具合を修正

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -17,33 +17,21 @@ class Admin::HomesController < ApplicationController
     # @posts_random = Post.where(is_deleted: false).where.not(customer_id: customer_ids).published.order("RAND()").limit(5)
     
     # ゲームの直近1週間のいいねランキングを取得
-    @posts_game = Post.where(genre_id: 1, is_deleted: false).where.not(customer_id: customer_ids).includes(:favorited_customers).published.limit(5).
-      sort {|a,b| 
-        b.favorited_customers.includes(:favorites).where(created_at: from...to).size <=> 
-        a.favorited_customers.includes(:favorites).where(created_at: from...to).size
-      }
+    @posts_game = Post.where(genre_id: 1, is_deleted: false).where.not(customer_id: customer_ids).published.last_week
       
     # ジャンル：ゲームに属する投稿からランダムに5件を取得
     @posts_game_random = Post.where(genre_id: 1, is_deleted: false).where.not(customer_id: customer_ids).published.order("RANDOM()").limit(5)
     # @posts_game_random = Post.where(genre_id: 1, is_deleted: false).where.not(customer_id: customer_ids).published.order("RAND()").limit(5)
     
     # 漫画の直近1週間のいいねランキングを取得
-    @posts_manga = Post.where(genre_id: 2, is_deleted: false).where.not(customer_id: customer_ids).includes(:favorited_customers).published.limit(5).
-      sort {|a,b| 
-        b.favorited_customers.includes(:favorites).where(created_at: from...to).size <=> 
-        a.favorited_customers.includes(:favorites).where(created_at: from...to).size
-      }
+    @posts_manga = Post.where(genre_id: 2, is_deleted: false).where.not(customer_id: customer_ids).published.last_week
       
     # ジャンル：漫画に属する投稿からランダムに5件を取得
     @posts_manga_random = Post.where(genre_id: 2, is_deleted: false).where.not(customer_id: customer_ids).order("RANDOM()").limit(5)
     # @posts_manga_random = Post.where(genre_id: 2, is_deleted: false).where.not(customer_id: customer_ids).order("RAND()").limit(5)
     
     # アプリの直近1週間のいいねランキングを取得
-    @posts_application = Post.where(genre_id: 3, is_deleted: false).where.not(customer_id: customer_ids).includes(:favorited_customers).limit(5).
-      sort {|a,b| 
-        b.favorited_customers.includes(:favorites).where(created_at: from...to).size <=> 
-        a.favorited_customers.includes(:favorites).where(created_at: from...to).size
-      }
+    @posts_application = Post.where(genre_id: 3, is_deleted: false).where.not(customer_id: customer_ids).published.last_week
       
     # ジャンル：アプリに属する投稿からランダムに5件を取得
     @posts_application_random = Post.where(genre_id: 3, is_deleted: false).where.not(customer_id: customer_ids).published.order("RANDOM()").limit(5)
