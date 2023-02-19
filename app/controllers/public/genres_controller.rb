ca95e4dd8be84b5c8c8ee6da80155ac4c0b6a455
EC2 Default User <ec2-user@ip-172-31-11-119.ap-northeast-1.compute.internal>
2023-02-19 10:58:42 +0000

    [Update]publicのタグ・検索投稿一覧にページネーション追加

--- app/controllers/public/genres_controller.rb
+++ app/controllers/public/genres_controller.rb
@@ -9,13 +9,13 @@ class Public::GenresController < ApplicationController
     # 退会済みの会員の投稿を非表示に
     customer_ids = Customer.where(is_deleted: true).pluck(:id)
     # 新着・いいね・コメント数順の並び替え
-    @posts = Post.where(genre_id: params[:id], is_deleted: false).where.not(customer_id: customer_ids).published.order(created_at: :desc)
+    @posts = Post.where(genre_id: params[:id], is_deleted: false).where.not(customer_id: customer_ids).published
     if params[:target] == "favorite"
       @posts = @posts.sort {|a,b| b.favorited_customers.size <=> a.favorited_customers.size}
-    elsif params[:target] == 'comment'
+    elsif params[:target] == "comment"
       @posts = @posts.sort {|a,b| b.commented_customers.size <=> a.commented_customers.size}
     else
-      @posts
+      @posts = @posts.order(created_at: :desc)
     end
     @posts = Kaminari.paginate_array(@posts).page(params[:page]).per(8)
   end
--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -8,13 +8,15 @@ class Public::SearchesController < ApplicationController
     else
       customer_ids = Customer.where(is_deleted: true).pluck(:id)
       # 新着・いいね・コメント数順の並び替え
+      @posts = Post.where(is_deleted: false).where.not(customer_id: customer_ids).published.looks(params[:search], params[:word])
       if params[:target] == "favorite"
-        @posts = Post.where(is_deleted: false).where.not(customer_id: customer_ids).published.looks(params[:search], params[:word]).sort {|a,b| b.favorited_customers.size <=> a.favorited_customers.size}
-      elsif params[:target] == 'comment'
-        @posts = Post.where(is_deleted: false).where.not(customer_id: customer_ids).published.looks(params[:search], params[:word]).sort {|a,b| b.commented_customers.size <=> a.commented_customers.size}
+        @posts = @posts.sort {|a,b| b.favorited_customers.size <=> a.favorited_customers.size}
+      elsif params[:target] == "comment"
+        @posts = @posts.sort {|a,b| b.commented_customers.size <=> a.commented_customers.size}
       else
-        @posts = Post.where(is_deleted: false).where.not(customer_id: customer_ids).published.looks(params[:search], params[:word]).order(created_at: :desc)
+        @posts = @posts.order(created_at: :desc)
       end
+      @posts = Kaminari.paginate_array(@posts).page(params[:page])
     end
   end
 end
