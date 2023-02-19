edde9634bd0e2dbde72f6c1ecb28f8bf9b3ab678
EC2 Default User <ec2-user@ip-172-31-11-119.ap-northeast-1.compute.internal>
2023-02-19 10:39:06 +0000

    [Update]publicのジャンル投稿一覧にページネーション追加

--- app/controllers/public/genres_controller.rb
+++ app/controllers/public/genres_controller.rb
@@ -9,13 +9,15 @@ class Public::GenresController < ApplicationController
     # 退会済みの会員の投稿を非表示に
     customer_ids = Customer.where(is_deleted: true).pluck(:id)
     # 新着・いいね・コメント数順の並び替え
+    @posts = Post.where(genre_id: params[:id], is_deleted: false).where.not(customer_id: customer_ids).published.order(created_at: :desc)
     if params[:target] == "favorite"
-      @posts = Post.where(genre_id: params[:id], is_deleted: false).where.not(customer_id: customer_ids).published.sort {|a,b| b.favorited_customers.size <=> a.favorited_customers.size}
+      @posts = @posts.sort {|a,b| b.favorited_customers.size <=> a.favorited_customers.size}
     elsif params[:target] == 'comment'
-      @posts = Post.where(genre_id: params[:id], is_deleted: false).where.not(customer_id: customer_ids).published.sort {|a,b| b.commented_customers.size <=> a.commented_customers.size}
+      @posts = @posts.sort {|a,b| b.commented_customers.size <=> a.commented_customers.size}
     else
-      @posts = Post.where(genre_id: params[:id], is_deleted: false).where.not(customer_id: customer_ids).published.order(created_at: :desc).page(params[:page])
+      @posts
     end
+    @posts = Kaminari.paginate_array(@posts).page(params[:page]).per(8)
   end
 
 end
